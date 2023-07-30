126ae27972e5953d12c8927ee779b503204c12c9
Keita <178keita1034@gmail.com>
2023-07-30 22:33:54 +0900

    component準備

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    // 下記以外の場所にあるbladeファイルはコンパイルする際に、自動で削除する
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
