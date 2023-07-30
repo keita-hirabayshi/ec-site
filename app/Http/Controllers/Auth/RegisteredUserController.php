56feb1a030131b52f567381ad7e48e9f0b4cb180
Keita <178keita1034@gmail.com>
2023-07-30 15:49:22 +0900

    日本語化対応

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
-
+// ユーザー登録
 class RegisteredUserController extends Controller
 {
     /**
