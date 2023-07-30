421df5df8474221fbf6de48c09c812d1ba4ec839
Keita <178keita1034@gmail.com>
2023-07-31 07:31:22 +0900

    属性バック

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
     test1
    <x-tests.card title="タイトル" content="本文" :message="$message" />
    <x-tests.card title="タイトル" />
+   <x-tests.card title="cssを変更したい" class="bg-red-200" />
 </x-tests.app>
