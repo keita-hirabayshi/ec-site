c13258b3164fb671f68abf5ec2a77c08be761757
Keita <178keita1034@gmail.com>
2023-08-06 16:09:25 +0900

    アプリ名、ロゴ

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-
+// プロジェクトのタイトル名を設定。 APP_NAMEは .envファイルにあり
     'name' => env('APP_NAME', 'Laravel'),
 
     /*
