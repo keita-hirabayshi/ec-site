0883e1b2442c432fb1042240ceed53ea840424d5
Keita <178keita1034@gmail.com>
2023-08-21 23:46:55 +0900

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -8,10 +8,16 @@ use InterventionImage;
 
 class ImageService{
     public static function upload($imageFile,$folderName){
+        if(is_array($imageFile)){
+            $file = $imageFile['image'];
+        }else{
+            $file = $imageFile;
+        }
+
         $fileName = uniqid(rand().'_');         //ファイル名作成
-        $extension = $imageFile->extension();   //拡張子の取得
+        $extension = $file->extension();   //拡張子の取得
         $fileNameToStore = $fileName. '.' . $extension; //画像データ
-        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode(); //リサイズ
+        $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode(); //リサイズ
         Storage::put('public/' . $folderName . '/' . $fileNameToStore,$resizedImage );
 
         return $fileNameToStore;
