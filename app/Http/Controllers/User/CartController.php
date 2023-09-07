4464b4a330888da9cf4a3808613c4841510732f3
Keita <178keita1034@gmail.com>
2023-09-07 23:36:24 +0900

    stripe作成途中

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -48,4 +48,38 @@ class CartController extends Controller
         
         return redirect()->route('user.cart.index');
     }
+
+    public function checkout(){
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+    // ユーザーに紐づく商品を全取得
+    // strpeに合わせて、データを格納
+    $lineItems = [];
+    foreach($products as $product){
+        $lineItem = [
+            'name' => $product->name,
+            'description' => $product->information,
+            'amount' => $product->price,
+            'currency' => 'jpy',
+            'quantity' => $product->pivot->quantity
+        ];
+        array_push($lineItems,$lineItem);
+    }
+    // dd($lineItems);
+    // strpeに合わせて、データを格納
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+ 
+        return view('user.checkout', compact('session', 'publicKey'));
+    }
 }
