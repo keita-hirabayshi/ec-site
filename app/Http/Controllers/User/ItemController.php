49596790c69fe25207fe1486a6890673ea6122de
Keita <178keita1034@gmail.com>
2023-09-12 22:41:10 +0900

    コンストラクタの修正

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -13,6 +13,19 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function($request, $next){
+            $id = $request->route()->parameter('item'); //itemのid取得
+        // 取得されてくる商品のIDは is_sellingが 1 のものだけなので
+            if(!is_null($id)){ // null判定
+                $itemId = Product::availableItems()->where('products.id',$id)->exists();
+                if(!$itemId){ // 同じでなかったら
+                  abort(404); // 404画面表示
+                }
+            }
+            // dd($request->route()->parameter('image'));
+            return$next($request);
+        });
     }
 
 
