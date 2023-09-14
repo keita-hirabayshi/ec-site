f4d40c3690361e6989ca4fea6fb11098ec86efaa
Keita <178keita1034@gmail.com>
2023-09-14 22:20:00 +0900

    表示順

--- app/Constants/Common.php
+++ app/Constants/Common.php
@@ -15,6 +15,19 @@ class Common
         'purchase' => self::PRODUCT_PURCHASE,
         'cancel' => self::PRODUCT_CANCEL,
     ];
+
+    const ORDER_RECOMMEND = '0';
+    const ORDER_HIGHER = '1';
+    const ORDER_LOWER = '2';
+    const ORDER_LATER = '3';
+    const ORDER_OLDER = '4';
+    const SORT_ORDER = [
+    'recommend' => self::ORDER_RECOMMEND,
+    'higherPrice' => self::ORDER_HIGHER,
+    'lowerPrice' => self::ORDER_LOWER,
+    'later' => self::ORDER_LATER,
+    'older' => self::ORDER_OLDER
+    ];
 }
 
 
