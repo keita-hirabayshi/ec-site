67f559a142fc49d3323d6558197c8779ed2728f2
Keita <178keita1034@gmail.com>
2023-09-19 22:21:27 +0900

    ユーザー向けメール送信

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Cart;
 use App\Models\Stock;
 use App\Services\CartService;
+use App\Jobs\SendThanksMail;
 
 class CartController extends Controller
 {
@@ -55,11 +56,13 @@ class CartController extends Controller
     // 
     $items = Cart::where('user_id',Auth::id())->get();
     $products = CartService::getItemsInCart($items);
-        
+    $user = User::findOrFail(Auth::id());
+    
+    SendThanksMail::dispatch($products,$user);
+    dd('ユーザーメール送信テスト');
     // 
-        $user = User::findOrFail(Auth::id());
-        $products = $user->products;
-        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
+    $products = $user->products;
+    $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
 
     // ユーザーに紐づく商品を全取得
     // 購入点数と在庫数を比較
--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -25,7 +25,7 @@ class CartService{
             
             array_push($products, $result); //配列に追加
         }   
-        dd($products);
+
         return $products;
     }
 }
