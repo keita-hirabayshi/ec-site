016073929ed6c33e62bc78b5f52164de2bc545a6
Keita <178keita1034@gmail.com>
2023-09-24 22:22:26 +0900

    add

--- app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Log;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -42,6 +43,8 @@ class AuthenticatedSessionController extends Controller
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
+
+        Log::debug('owner',$request->session()->all());
 
         return redirect('/owner/login');
     }
